a0ce5477b1bb336deca4c6e90d9a3a6172f0719e
kimsanghyeon <sanghyeon2420@kakao.com>
2023-02-09 11:28:35 +0900

    프로젝트 세팅

---
+++ src/test/java/me/whiteship/demospringjpa/DemospringjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
